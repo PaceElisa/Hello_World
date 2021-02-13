00c0ae9873e23d35f45d15f8cfa088c7b327fca2
Elisa Pace <s1084405@studenti.univpm.it>
2021-02-13 19:17:02 +0100

    creazione del metodo getPromoter che gestiste la rotta get /promoter e i metodo che gestistono le eccezioni WrongStateCodeException e NoPromoterException mandando delle risposte di BAD_REQUEST

--- src/main/java/it/univpm/TicketMasterApp/controller/ControllerApp.java
+++ src/main/java/it/univpm/TicketMasterApp/controller/ControllerApp.java
@@ -3,10 +3,52 @@
  */
 package it.univpm.TicketMasterApp.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import it.univpm.TicketMasterApp.exception.NoPromoterException;
+import it.univpm.TicketMasterApp.exception.WrongStateCodeException;
+import it.univpm.TicketMasterApp.service.EventService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
- * @author Marco
+ * @author Marco Vassallo
+ * @author Elisa Pace
  *
  */
+@RestController
 public class ControllerApp {
+	
+	@Autowired
+	EventService e;
+	
+	@GetMapping("/promoter")
+	public ResponseEntity<Object> getPromoter(@RequestParam(name="stateCode", defaultValue= "MB") String stateCode) throws NoPromoterException, WrongStateCodeException
+	{
+		return new ResponseEntity<>(e.getPromoter(stateCode), HttpStatus.OK);
+	}
+	
+	@ExceptionHandler(WrongStateCodeException.class)
+	public ResponseEntity<Object> handleIOException(WrongStateCodeException err){
+		return new ResponseEntity<> (err.StateCodeError(),HttpStatus.BAD_REQUEST);
+		
+	}
+	
+	@ExceptionHandler(NoPromoterException.class)
+	public ResponseEntity<Object> handleIOException(NoPromoterException err){
+		return new ResponseEntity<> (err.PromoterError(),HttpStatus.BAD_REQUEST);
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
