5d64226074622076699bd8acf72e1374289fd480
Elisa Pace <s1084405@studenti.univpm.it>
2021-02-14 22:42:11 +0100

    Inserimento rotta stasReg

--- src/main/java/it/univpm/TicketMasterApp/controller/ControllerApp.java
+++ src/main/java/it/univpm/TicketMasterApp/controller/ControllerApp.java
@@ -3,18 +3,19 @@
  */
 package it.univpm.TicketMasterApp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 import it.univpm.TicketMasterApp.exception.NoPromoterException;
 import it.univpm.TicketMasterApp.exception.WrongStateCodeException;
 import it.univpm.TicketMasterApp.service.EventService;
+import it.univpm.TicketMasterApp.model.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Marco Vassallo
@@ -27,8 +28,9 @@ public class ControllerApp {
 	@Autowired
 	EventService e;
 	
+	
 	@GetMapping("/promoter")
-	public ResponseEntity<Object> getPromoter(@RequestParam(name="stateCode", defaultValue= "MB") String stateCode) throws NoPromoterException, WrongStateCodeException
+	public ResponseEntity<Object> getpromoter(@RequestParam(name="stateCode", defaultValue= "MB") String stateCode) throws NoPromoterException, WrongStateCodeException
 	{
 		return new ResponseEntity<>(e.getPromoter(stateCode), HttpStatus.OK);
 	}
@@ -44,6 +46,13 @@ public class ControllerApp {
 		return new ResponseEntity<> (err.PromoterError(),HttpStatus.BAD_REQUEST);
 		
 	}
+	
+	@RequestMapping(value="statsReg")
+	public ResponseEntity<Object> getStasRegion()
+	{
+		return new ResponseEntity<>(e.StatsReg().toString(),HttpStatus.OK);
+	}
+	
 	
 	
 	
