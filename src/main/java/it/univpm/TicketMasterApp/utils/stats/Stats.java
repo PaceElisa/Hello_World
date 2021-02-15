8a95523b98c63c814ea20ee07e7b31a6e33b73c1
Elisa Pace <s1084405@studenti.univpm.it>
2021-02-15 23:10:04 +0100

    Aggiunta commenti

--- src/main/java/it/univpm/TicketMasterApp/utils/stats/Stats.java
+++ src/main/java/it/univpm/TicketMasterApp/utils/stats/Stats.java
@@ -9,20 +9,30 @@ import org.json.simple.JSONObject;
 
 import it.univpm.TicketMasterApp.model.Eventi;
 
-/**
+/**Superclasse che calcola le statistiche
  * @author Elisa Pace
  * @Author Marco Vassallo
  *
  */
 public abstract class Stats {
-	
+	/**
+	 * Vettore di eventi che contiene i dati su cui calcolare le statistiche
+	 */
 	 Vector<Eventi> eventi=new Vector<>();
+	 /**
+	  * parametro che mi indica su che cosa sto facendo la statistica
+	  */
+	 String parametro;
 	
 	
-	
-	public Stats(Vector<Eventi> eventi) {
+	/**
+	 * Costruttore
+	 * @param eventi  Vettore di eventi che contiene i dati su cui calcolare le statistiche
+	 * @param parametro  parametro che mi indica su che cosa sto facendo la statistica
+	 */
+	public Stats(Vector<Eventi> eventi,String parametro) {
 		super();
-		this.eventi = eventi;
+		
 		
 	}
 	
