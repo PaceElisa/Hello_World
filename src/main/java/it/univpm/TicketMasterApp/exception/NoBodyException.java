1b13fd80c70943d5c114269334cf7852660af477
Elisa Pace <s1084405@studenti.univpm.it>
2021-02-16 00:41:05 +0100

    Creazione classe NoBodyException

---
+++ src/main/java/it/univpm/TicketMasterApp/exception/NoBodyException.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package it.univpm.TicketMasterApp.exception;
+
+/** Classe che viene lanciata quando si verifica un eccezione 
+ * riguardo lassenza del body della richiesta
+ * @author Elisa Pace
+ * @author Marco Vassallo
+ *
+ */
+public class NoBodyException extends Exception {
+
+	/**
+	 * Costruttore
+	 */
+	public NoBodyException() {
+		super("Errore: non è stato inserito nessun body...");
+	}
+
+	/**Metodo lanciato dalla classe ControllerApp
+	 * @param message
+	 * @return un messaggio di errore che mostra un esempio del corpo da inserire
+	 */
+	public String BodyError(String message) {
+		return("E' richiesto un body di questo tipo:\n"
+				+ "{\r\n"
+				+ "    \"promoter\": [\r\n"
+				+ "        {\r\n"
+				+ "            \"ID\": \"850\"\r\n"
+				+ "        },\r\n"
+				+ "        {\r\n"
+				+ "            \"ID\": \"653\"\r\n"
+				+ "        },\r\n"
+				+ "        {\r\n"
+				+ "            \"ID\": \"494\"\r\n"
+				+ "        }\r\n"
+				+ "    ]\r\n"
+				+ "}");
+		}
+
+	
+}
