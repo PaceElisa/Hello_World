ef6afbec4f903075422ef113c0bfe60bdbfc605a
Elisa Pace <s1084405@studenti.univpm.it>
2021-02-15 12:36:27 +0100

    Modifica classe getPromoter nel ciclo for e riposizionamento Vector<Promoter> giusti

--- src/main/java/it/univpm/TicketMasterApp/service/EventServiceImpl.java
+++ src/main/java/it/univpm/TicketMasterApp/service/EventServiceImpl.java
@@ -4,31 +4,39 @@
 package it.univpm.TicketMasterApp.service;
 
 import java.util.Vector;
-
+import org.springframework.stereotype.Service;
 import it.univpm.TicketMasterApp.exception.NoPromoterException;
 import it.univpm.TicketMasterApp.exception.WrongStateCodeException;
 import it.univpm.TicketMasterApp.model.Promoter;
 
-/**
+/**Classe che gestisce i metodi chiamati dalla classe ControllerApp
  * @author Elisa Pace
  * @author Marco Vassallo
  *
  */
+@Service
 public class EventServiceImpl implements EventService {
-	Vector<Promoter> giusti= new Vector<>();
 	
-	public EventServiceImpl() {}	
+	
+	/**
+	 * Costruttore vuoto
+	 */
+	public EventServiceImpl() {}
+	
+	/**@param
+	 * @return
+	 */
 	public Vector<Promoter> getPromoter(String stateCode) throws WrongStateCodeException, NoPromoterException{
-		
+		Vector<Promoter> giusti= new Vector<>();
 		//giusti=null;
 		DownloadEvent e= new DownloadEvent();
 		
 		if(e.Associa(stateCode)==null) throw new WrongStateCodeException();
 		e.EventiInfo(stateCode);
-		//controllo che i suoi elementi non siano settati a null o che l'id del promoter sia settato a id"320", indicano i promoter non definiti
+		//controllo che i suoi elementi non siano settati a "" o che l'id del promoter sia settato a id"320", indicano i promoter non definiti
 		//poi aggiunge in giusti solo elementi non presenti gia al suo interno
 		for(Promoter p: e.getListaProm() ) {//scorro lil vettore contente oggetti di tipo promoter
-			if((!(p.getID() ==null) )&& (!(p.getID()=="320"))) {
+			if(!(p.getID().equals("") || p.getID().equals("320"))) {
 				if(!(giusti.contains(p)))
 					giusti.add(p);
 			}
