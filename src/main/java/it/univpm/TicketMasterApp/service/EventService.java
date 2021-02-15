6ba50d08ab08e06af7f1bb4dd987706a984dfb33
Elisa Pace <s1084405@studenti.univpm.it>
2021-02-15 20:27:24 +0100

    aggiunta commenti

--- src/main/java/it/univpm/TicketMasterApp/service/EventService.java
+++ src/main/java/it/univpm/TicketMasterApp/service/EventService.java
@@ -5,12 +5,12 @@ package it.univpm.TicketMasterApp.service;
 import java.util.Vector;
 
 import org.json.simple.JSONArray;
-import org.springframework.stereotype.Service;
+
 
 import it.univpm.TicketMasterApp.exception.NoPromoterException;
 import it.univpm.TicketMasterApp.exception.WrongStateCodeException;
 import it.univpm.TicketMasterApp.model.Promoter;
-/**
+/**Interfaccia che contiene i metodo chiamati dal ControllerApp
  * @author  Elisa Pace
  * @author Marco Vassallo
  *
@@ -19,7 +19,7 @@ import it.univpm.TicketMasterApp.model.Promoter;
 public interface EventService {
 	
 	public  abstract Vector<Promoter> getPromoter(String stateCode) throws WrongStateCodeException, NoPromoterException;
-	//public  abstract JSONArray StatsReg();
+	public  abstract JSONArray StatsRegion();
 	//public  abstract JSONArray  StatsProm(String id);
 
 }
