0c55c58335276c7ed71ad18986eb8f55ae346776
Elisa Pace <s1084405@studenti.univpm.it>
2021-02-16 01:06:20 +0100

    inserimento metodo getStasPromoter, con le relative eccezioni

--- src/main/java/it/univpm/TicketMasterApp/controller/ControllerApp.java
+++ src/main/java/it/univpm/TicketMasterApp/controller/ControllerApp.java
@@ -3,13 +3,22 @@
  */
 package it.univpm.TicketMasterApp.controller;
 
+import it.univpm.TicketMasterApp.exception.NoBodyException;
+
 //import org.springframework.web.bind.annotation.RestController;
 
 import it.univpm.TicketMasterApp.exception.NoPromoterException;
+import it.univpm.TicketMasterApp.exception.WrongIDExceotion;
 import it.univpm.TicketMasterApp.exception.WrongStateCodeException;
 import it.univpm.TicketMasterApp.service.EventService;
 import it.univpm.TicketMasterApp.model.*;
 
+import java.util.Vector;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,13 +55,35 @@ public class ControllerApp {
 		return new ResponseEntity<> (err.PromoterError(),HttpStatus.BAD_REQUEST);
 		
 	}
-/*	
+	
 	@RequestMapping(value="statsReg")
 	public ResponseEntity<Object> getStasRegion()
 	{
-		return new ResponseEntity<>(e.StatsReg().toString(),HttpStatus.OK);
+		return new ResponseEntity<>(e.StatsRegion().toString(),HttpStatus.OK);
 	}
-	*/
+	
+	@PostMapping(value="statsProm")
+	public ResponseEntity<Object> getStatsPromoter(@RequestBody JSONObject body) throws NoBodyException, WrongIDExceotion
+	{
+		Vector<String> id_promoter=new Vector<String>(body.size());
+		JSONArray vector=(JSONArray)body.get("promoter");
+		
+		for(Object o: vector) {
+			JSONObject object=(JSONObject)o;
+			id_promoter.add((String) object.get("ID"));
+			}
+		return new ResponseEntity<>(e.StatsPromoter(id_promoter),HttpStatus.OK);
+	}
+	
+	@ExceptionHandler(NoBodyException.class)
+	public ResponseEntity<Object> handleIOException(NoBodyException err){
+		return new ResponseEntity<> (err.BodyError(),HttpStatus.BAD_REQUEST);
+	}
+	@ExceptionHandler(WrongIDExceotion.class)
+	public ResponseEntity<Object> handleIOException(WrongIDExceotion err){
+		return new ResponseEntity<> (err.IDError(),HttpStatus.BAD_REQUEST);
+	}
+		
 	
 	
 	
