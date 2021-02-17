9d5a39e28e23630b29906829ff1d1c4550b48d01
Marco Vassallo <s1088826@studenti.univpm.it>
2021-02-17 18:31:47 +0100

    Aggiunta Commento

---
+++ src/test/java/it/univpm/TicketMasterApp/service/StatsPromoterTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package it.univpm.TicketMasterApp.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Vector;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import it.univpm.TicketMasterApp.exception.EmptyIDException;
+
+/**
+ * @author Elisa Pace
+ * @author Marco Vassallo
+ * Questa classe si occupa della corretta gestione della classe StatsPromoter
+ */
+class StatsPromoterTest {
+
+	Vector<String> promoterbody;
+	EventServiceImpl service;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		promoterbody = new Vector<>();
+		service = new EventServiceImpl();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	@DisplayName("Corretto funzionamento di StatsPromoter.")
+	void test() {
+		promoterbody.add("850");
+		promoterbody.add(null);
+		assertThrows(EmptyIDException.class, ()-> service.StatsPromoter(promoterbody));
+	}
+
+}
