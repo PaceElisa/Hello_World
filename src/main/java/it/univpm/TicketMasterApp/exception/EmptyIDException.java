d36239c41695ae890fdfda6c527982427ec5bc90
Elisa Pace <s1084405@studenti.univpm.it>
2021-02-18 12:03:05 +0100

    aggiunta di metodi per la gestione delle eccezioni della rotta filter, più commenti

--- src/main/java/it/univpm/TicketMasterApp/exception/EmptyIDException.java
+++ src/main/java/it/univpm/TicketMasterApp/exception/EmptyIDException.java
@@ -3,7 +3,7 @@
  */
 package it.univpm.TicketMasterApp.exception;
 
-/**
+/**Metodo che viene lanciato se il body della mia richiesta è vuoto
  * @author Elisa Pace
  * @author Marco Vassallo
  *
@@ -11,23 +11,40 @@ package it.univpm.TicketMasterApp.exception;
 public class EmptyIDException extends Exception {
 
 	/**
-	 * 
+	 * messaggio passato quando viene lanciata l'eccezione
 	 */
+	String messaggio;
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * 
+	 * costruttore
 	 */
 	public EmptyIDException() {
 		super("Errore: campo vuoto nel corpo della richiesta...");
 		
 	}
 
-	/**
+	/** costruttore con :
 	 * @param message
+	 */
+	public EmptyIDException(String messaggio) {
+		this.messaggio=messaggio;
+		
+	}
+
+	/**
+	 *Messaggio di errore 
+	 *@return un messaggio di errore prefissato per la gestione della rotta statsProm
 	 */
 	public String EmptyIDError() {
 		return("E' necessario riempire tutti i campi del corpo per un corretto funzionamento...");
+	}
+	/**Messaggio di errore
+	 * @return  ritorna un messaggio di errore scritto dal programmatore per la gestione della rotta filter
+	 */
+	
+	public String Messaggio() {
+		return messaggio;
 	}
 
 }
